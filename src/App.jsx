31e001aa7d3dfd43d8327832927231c7bc636bb8
iCode_Rx <arababs2015@gmail.com>
2021-10-19 19:49:16 +0100

    toggle added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import "./App.css"
+
 import Intro from "./components/intro/Intro"
 import About from "./components/about/About"
-import "./App.css"
+import Toggle from "./components/toggle/Toggle";
+
+import { useContext } from "react";
+import { ThemeContext } from "./context";
 
 
 const App = () => {
+	const theme = useContext(ThemeContext)
+	console.log(theme)
+	const darkMode = theme.state.darkMode
 
 
 	return (
-		<div className="App">
+		<div 
+			style={
+				{
+					backgroundColor: darkMode ? "#222" : "white",
+					color: darkMode && "white"
+				}
+			}
+		>
+			<Toggle />
 			<Intro />
 			<About />
 		</div>
